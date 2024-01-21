4a66148fef4f855f699f839a01f025af6dfbdcb9
Neil Ryan <neilryanlipaod@gmail.com>
2024-01-21 12:24:11 +0800

    Add test and logging dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation ("io.github.oshai:kotlin-logging-jvm:5.1.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "mockito-core")
+	}
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	testImplementation("com.ninja-squad:springmockk:4.0.0")
+	testImplementation("com.willowtreeapps.assertk:assertk:0.28.0")
 }
 
 tasks.withType<KotlinCompile> {
